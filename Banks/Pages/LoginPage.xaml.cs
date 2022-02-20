86197ed800267e2bc6c0fa9255451e08f8c9f0d6
Куветов Даниил <kuvetov@bk.ru>
2022-02-20 09:35:37 +0300

    Дополнил стиль кнопки, переименовал стиль

--- Banks/Pages/LoginPage.xaml.cs
+++ Banks/Pages/LoginPage.xaml.cs
@@ -30,5 +30,14 @@ namespace Banks.Pages
         {
             MessageBox.Show("ЬЫП");
         }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void ent_MouseEnter(object sender, MouseEventArgs e)
+        {
+        }
     }
 }
